e9f39d2f508bad395360d4ad70408cd93629f7c5
XuanSon99 <nguyensonn124@gmail.com>
2022-12-25 18:27:37 +0700

    change

--- index.php
+++ index.php
@@ -1,29 +1,11 @@
-<!DOCTYPE html>
-<html>
+<?php
+$path = "https://api.telegram.org/bot5940983546:AAFQ7g6gL_r6Tp6LG1je1fkMXHZbja9RBsk";
 
-<body>
+$update = json_decode(file_get_contents("php://input"), TRUE);
 
-    <?php
-    $update = json_decode(file_get_contents("php://input"), TRUE);
-    $chatId = $update["message"]["chat"]["id"];
-    $message = $update["message"]["text"];
-    $token = '5940983546:AAFQ7g6gL_r6Tp6LG1je1fkMXHZbja9RBsk';
+$chatId = $update["message"]["chat"]["id"];
+$message = $update["message"]["text"];
 
-    if (strpos($message, "/xinchao") === 0) {
-        $response = "Chào bạn, BOT có thể giúp gì? \n";
-
-        $curl = curl_init();
-
-        curl_setopt_array($curl, array(
-            CURLOPT_RETURNTRANSFER => 1,
-            CURLOPT_URL => 'https://api.telegram.org/bot' . $token . '/sendMessage?chat_id=' . $chatId . '&text=' . $response,
-            CURLOPT_POST => 1,
-            CURLOPT_SSL_VERIFYPEER => false
-        ));
-        curl_close($curl);
-    }
-    ?>
-
-</body>
-
-</html>
+if (strpos($message, "/start") === 0) {
+    file_get_contents($path."/sendmessage?chat_id=".$chatId."&text=Here's the weather in");
+}
